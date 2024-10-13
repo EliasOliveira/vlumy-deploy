7135c3b588c752559485c75b7044715d543be644
EliasOliveira <jun.oliveira10@gmail.com>
2024-10-13 13:47:48 +0100

    Testing

--- install/build.sh
+++ install/build.sh
@@ -1,6 +1,10 @@
-FLUTTER="/Users/junioroliveira/Documents/development/flutter/bin//flutter"
+FLUTTER="/Users/junioroliveira/Documents/development/flutter/bin/flutter"
 
-ls $FLUTTER
-$FLUTTER --version
+cd flutter-app
 
+$FLUTTER pub get && \
+$FLUTTER pub run flutter_launcher_icons && \
+$FLUTTER packages pub run build_runner build --delete-conflicting-outputs && \
+$FLUTTER build appbundle && \
+$FLUTTER build apk
 
