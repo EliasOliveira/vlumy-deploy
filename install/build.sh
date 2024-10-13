4a0f87d74ffc86253e0a51dc47bccf17b203a609
EliasOliveira <jun.oliveira10@gmail.com>
2024-10-13 14:30:47 +0100

    Testing

--- install/build.sh
+++ install/build.sh
@@ -6,7 +6,10 @@ $FLUTTER pub get && \
 $FLUTTER pub run flutter_launcher_icons && \
 $FLUTTER packages pub run build_runner build --delete-conflicting-outputs && \
 $FLUTTER build appbundle && \
-$FLUTTER build apk
+$FLUTTER build apk && \
+$FLUTTER build ipa
 
-cd build\app\outputs\bundle\release\app-release.aab ..\install
-cd build\app\outputs\flutter-apk\app-release.apk ..\install
+cp build/app/outputs/bundle/release/app-release.aab ../install
+cp build/app/outputs/flutter-apk/app-release.apk ../install
+cp -r build/ios ../install
+
